95f776370a6997b13b9995cd3e3356c72562bcd7
Varun Mathur <varunmathur1990@gmail.com>
2021-09-27 19:14:50 +0530

    Update base_options.py

--- options/base_options.py
+++ options/base_options.py
@@ -12,7 +12,7 @@ class BaseOptions():
     def initialize(self):
         self.parser.add_argument('--dataroot', required=True, help='path to images (should have subfolders trainA, trainB, trainC, etc)')
         self.parser.add_argument('--batchSize', type=int, default=1, help='input batch size')
-        self.parser.add_argument('--loadSize', type=int, default=512, help='scale images to this size')
+        self.parser.add_argument('--loadSize', type=int, default=256, help='scale images to this size')
         self.parser.add_argument('--input_nc', type=int, default=6, help='# of input image channels')
         self.parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels')
         self.parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in first conv layer')
@@ -22,7 +22,7 @@ class BaseOptions():
         self.parser.add_argument('--n_layers_D', type=int, default=5, help='only used if which_model_netD==n_layers')
         self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         self.parser.add_argument('--name', type=str, default='reflection_synthesis', help='name of the experiment. It decides where to store samples and models')
-        self.parser.add_argument('--nThreads', default=8, type=int, help='# threads for loading data')
+        self.parser.add_argument('--nThreads', default=2, type=int, help='# threads for loading data')
         self.parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints_synthesis', help='models are saved here')
         self.parser.add_argument('--norm', type=str, default='instance', help='instance normalization or batch normalization')
         self.parser.add_argument('--serial_batches', action='store_false', help='if true, takes images in order to make batches, otherwise takes them randomly')
