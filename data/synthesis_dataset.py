cf65e1cdedc7ae994e4c22d1b4667849239268a8
Varun Mathur <varunmathur1990@gmail.com>
2022-01-09 20:41:38 +0530

    New mask

--- data/synthesis_dataset.py
+++ data/synthesis_dataset.py
@@ -84,7 +84,17 @@ class SynthesisDataset(BaseDataset):
         # for different reflection types
         # Focused reflection
         if reflection_type == 'focused':
-            pass
+            A_img = np.asarray(A_img)
+            B_img = np.asarray(B_img)
+            # Generate blend masks, here: linear, horizontal fading from 1 to 0.6 and from 0 to 0.4
+            mask1 = np.repeat(np.tile(np.linspace(1, 0.6, A_img.shape[1]), (A_img.shape[0], 1))[:, :, np.newaxis], 3, axis=2)
+            mask2 = np.repeat(np.tile(np.linspace(0, 0.4, A_img.shape[1]), (A_img.shape[0], 1))[:, :, np.newaxis], 3, axis=2)
+            A_img = A_img * mask1
+            B_img = B_img * mask2
+            # Generate output by linear blending
+            A_img = Image.fromarray(A_img.astype(np.uint8))
+            B_img = Image.fromarray(B_img.astype(np.uint8))
+
         # Defocused reflection
         elif reflection_type == 'defocused':
             A_img = np.asarray(A_img)
@@ -106,17 +116,9 @@ class SynthesisDataset(BaseDataset):
             A_img = transforms.functional.crop(A_img, shift_y, shift_x, cols-shift_y, rows-shift_x)
             A_img = A_img.resize((rows, cols), Image.BILINEAR)
         
-        A_img = np.asarray(A_img)
         A = self.get_crop_resized(A_img)
         A_origin = self.get_crop_resized(A_img_origin)
         B = self.get_resized(B_img)
-        # Generate blend masks, here: linear, horizontal fading from 1 to 0 and from 0 to 1
-        mask1 = np.repeat(np.tile(np.linspace(1, 0.6, A_img.shape[1]), (A_img.shape[0], 1))[:, :, np.newaxis], 3, axis=2)
-        mask2 = np.repeat(np.tile(np.linspace(0, 0.4, A_img.shape[1]), (A_img.shape[0], 1))[:, :, np.newaxis], 3, axis=2)
-
-        # Generate output by linear blending
-        final = np.uint8(A_img * mask1 + B_img * mask2)
-
 
         if self.opt.phase == 'train':
             index_C = random.randint(0, self.C_size - 1)
